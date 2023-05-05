f2f31a75689594864ed1d88115baa6179e1317fc
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-05 16:57:21 +0300

    feat(backend): added static-files lib, fixed tsconfig files

--- projects/backend/host/src/host.module.ts
+++ projects/backend/host/src/host.module.ts
@@ -1,7 +1,11 @@
 import { Module } from "@nestjs/common";
+import { ComicModule } from "./modules";
+import { ConfigurationModule } from "@cjp/configuration";
+import { MongoModule } from "@cjp/mongo";
+import { StaticFilesModule } from "@cjp/static-files";
 
 @Module({
-  imports: [],
+  imports: [ConfigurationModule, MongoModule, ComicModule, StaticFilesModule],
   controllers: [],
   providers: [],
 })
