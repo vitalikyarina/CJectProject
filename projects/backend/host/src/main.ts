eba638b3fb240eb11b855a181622a888924bc383
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-05 16:52:25 +0300

    feat(backend-host): added swagger

--- projects/backend/host/src/main.ts
+++ projects/backend/host/src/main.ts
@@ -4,12 +4,23 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestFastifyApplication>(
     HostModule,
     new FastifyAdapter(),
   );
+  const globalPrefix = "api";
+  app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle("CJect example")
+    .setDescription("The CJect API description")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
   await app.listen(3000);
 }
 bootstrap();
