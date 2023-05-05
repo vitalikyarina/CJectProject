d5d6151618600b15d76379c44018d35408379c66
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-05 15:04:50 +0300

    feat(host): added header and side naw

--- projects/frontend/host/src/app/app.routes.ts
+++ projects/frontend/host/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from "@angular/router";
+import { AppRoute } from "./enums";
 
 export const routes: Routes = [
   {
-    path: "comic",
+    path: AppRoute.COMICS,
     loadChildren: () => import("comic/Routes").then((m) => m.ROUTES),
+  },
+  {
+    path: "**",
+    redirectTo: AppRoute.DEFAULT,
   },
 ];
