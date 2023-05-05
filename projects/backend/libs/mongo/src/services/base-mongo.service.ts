980de0fa723cec0eac139510388bd44c0d940407
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-05 16:48:56 +0300

    feat(backend): added mongoose, added shared and mongo back lib

---
+++ projects/backend/libs/mongo/src/services/base-mongo.service.ts
@@ -0,0 +1,42 @@
+import { FilterQuery } from "mongoose";
+import { Observable } from "rxjs";
+import { BaseMongoAPIService } from "../api";
+import { IBaseDatabaseDAO, IFindOptions } from "@cjp/shared";
+
+export class BaseMongoService<TDocument, TCreate, TUpdate>
+  implements IBaseDatabaseDAO<TDocument, TCreate, TUpdate>
+{
+  constructor(
+    private readonly baseApi: BaseMongoAPIService<TDocument, TCreate, TUpdate>,
+  ) {}
+
+  public createOne(newDocument: TCreate): Observable<TDocument> {
+    return this.baseApi.createOne(newDocument);
+  }
+
+  public find(
+    queryConditions?: FilterQuery<TDocument>,
+    options?: IFindOptions,
+  ): Observable<TDocument> {
+    return this.baseApi.find(queryConditions, options);
+  }
+
+  public findById(id: string): Observable<TDocument> {
+    return this.baseApi.findById(id);
+  }
+
+  public deleteOne(id: string): Observable<TDocument> {
+    return this.baseApi.deleteOne(id);
+  }
+
+  public updateOne(
+    queryConditions: FilterQuery<TDocument>,
+    updateData: TUpdate,
+  ): Observable<TDocument> {
+    return this.baseApi.updateOne(queryConditions, updateData);
+  }
+
+  public updateOneById(id: string, updateData: TUpdate): Observable<TDocument> {
+    return this.baseApi.updateOneById(id, updateData);
+  }
+}
---
+++ projects/backend/libs/shared/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./interfaces";
+export * from "./types";
+export * from "./observables";
+export * from "./services";
+export * from "./shared.module";
