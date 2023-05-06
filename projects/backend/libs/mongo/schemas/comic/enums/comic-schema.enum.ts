dc296d6166c82338f37608676dd26f6d3879753f
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-06 20:47:13 +0300

    refactor(backend-mongo-lib): changed naming

--- projects/backend/libs/mongo/schemas/comic/enums/comic-schema.enum.ts
+++ projects/backend/libs/mongo/schemas/comic/enums/comic-schema.enum.ts
@@ -1,8 +1,8 @@
 export const ComicSchema = {
   COMIC: "comic",
-  COMICS_SITE: "comics_site",
-  COMICS_CHAPTER: "comics_chapter",
-  COMICS_RESOURCE: "comics_resource",
+  COMICS_SITE: "comic_sites",
+  COMICS_CHAPTER: "comic_chapters",
+  COMICS_RESOURCE: "comic_resources",
 } as const;
 
 export type ComicSchema = (typeof ComicSchema)[keyof typeof ComicSchema];
