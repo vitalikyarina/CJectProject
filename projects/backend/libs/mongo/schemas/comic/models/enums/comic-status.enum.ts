76f37cf070981bfe0105b6485080fd0b343394cf
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-06 20:57:06 +0300

    feat(shared-comic): added comic interface

--- projects/backend/libs/mongo/schemas/comic/models/enums/comic-status.enum.ts
+++
@@ -1,7 +0,0 @@
-export const ComicStatus = {
-  ONGOING: "ONGOING",
-  COMPLETE: "COMPLETE",
-  BAD: "BAD",
-} as const;
-
-export type ComicStatus = (typeof ComicStatus)[keyof typeof ComicStatus];
--- projects/backend/libs/mongo/schemas/comic/models/enums/comic-tag.enum.ts
+++
@@ -1,3 +0,0 @@
-export const ComicTag = {} as const;
-
-export type ComicTag = (typeof ComicTag)[keyof typeof ComicTag];
