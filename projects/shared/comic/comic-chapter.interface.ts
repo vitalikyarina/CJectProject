5628ee7a88e430d7e233740b69c88a7701579e62
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-06 20:47:58 +0300

    feat(shared-comic): added comic chapter interface

---
+++ projects/shared/comic/comic-chapter.interface.ts
@@ -0,0 +1,12 @@
+import { ComicError } from "./enums";
+
+export interface IChapter {
+  number: number;
+  link: string;
+  date: number;
+  resource: unknown;
+  countPage: number;
+  isLoaded: boolean;
+  errorPages: Record<number, boolean>;
+  errorType: ComicError;
+}
--- projects/shared/comic/index.ts
+++ projects/shared/comic/index.ts
@@ -1,3 +1,4 @@
 export * from "./comic-site.interface";
 export * from "./comic-resource.interface";
+export * from "./comic-chapter.interface";
 export * from "./enums";
