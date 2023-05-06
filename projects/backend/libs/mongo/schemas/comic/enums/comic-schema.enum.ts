56224e998a0d5272434e7fc8e982d54d79d05a53
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-06 22:11:01 +0300

    feat(backend-mongo-comic): added comic schema, api, service, model

--- projects/backend/libs/mongo/schemas/comic/enums/comic-schema.enum.ts
+++
@@ -1,8 +0,0 @@
-export const ComicSchema = {
-  COMIC: "comic",
-  COMICS_SITE: "comic_site",
-  COMICS_CHAPTER: "comic_chapter",
-  COMICS_RESOURCE: "comic_resource",
-} as const;
-
-export type ComicSchema = (typeof ComicSchema)[keyof typeof ComicSchema];
