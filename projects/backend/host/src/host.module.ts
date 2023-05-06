3df09a4712a6c199bd6dbe8cbe268a97d0396a28
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-06 18:26:52 +0300

    fix(backend-host): fixed host.module alias

--- projects/backend/host/src/host.module.ts
+++ projects/backend/host/src/host.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
 import { ComicModule } from "./modules";
-import { ConfigurationModule } from "@cjp/configuration";
-import { MongoModule } from "@cjp/mongo";
-import { StaticFilesModule } from "@cjp/static-files";
+import { ConfigurationModule } from "@cjp-back/configuration";
+import { MongoModule } from "@cjp-back/mongo";
+import { StaticFilesModule } from "@cjp-back/static-files";
 
 @Module({
   imports: [ConfigurationModule, MongoModule, ComicModule, StaticFilesModule],
