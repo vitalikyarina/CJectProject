47ef4039b5c85368887d5b10e0cd8f135c64b7f2
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-06 18:24:57 +0300

    feat(shared): added comic-site interface

---
+++ projects/shared/comic/comic-site.interface.ts
@@ -0,0 +1,12 @@
+export interface IComicSite {
+  mainImagePath: string;
+  chaptersPath: string;
+  chapterNamePath: string;
+  chapterDatePath: string;
+  chapterImagesPath: string;
+  chapterLazyLoadPath: string | undefined | null;
+  browserType: string | undefined | null;
+  name: string;
+  baseLink: string;
+  dateFormat: string;
+}
