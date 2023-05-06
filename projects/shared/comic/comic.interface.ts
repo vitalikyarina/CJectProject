3009e919621587efe75f6b8538b7d1d56326d28c
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-06 20:57:06 +0300

    feat(shared-comic): added comic interface

---
+++ projects/shared/comic/comic.interface.ts
@@ -0,0 +1,11 @@
+import { ComicStatus, ComicTag } from "./enums";
+
+export interface IComic {
+  name: string;
+  altNames: string[];
+  resources: unknown[];
+  chapters: unknown[];
+  latestUpdate: number;
+  tags: ComicTag[];
+  status: ComicStatus;
+}
