184f958c18be157a4c5d717a114f170c39c1dae1
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-06 21:51:53 +0300

    fix(backend): fixed fastify adapter

--- projects/backend/parsing/src/main.ts
+++ projects/backend/parsing/src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { ParsingModule } from "./parsing.module";
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from "@nestjs/platform-fastify";
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    ParsingModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(ParsingModule);
   await app.listen(3000);
 }
 bootstrap();
