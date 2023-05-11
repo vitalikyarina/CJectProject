dbb4a977f3c37bc094fd3e08d31a4a92c6eca45c
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-11 14:12:54 +0300

    feat(frontend): added content section lib

---
+++ projects/frontend/libs/content-section/src/lib/components/content-section-action/content-section-action.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "cjp-content-section-action",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./content-section-action.component.html",
+  styleUrls: ["./content-section-action.component.scss"],
+})
+export class ContentSectionActionComponent {}
