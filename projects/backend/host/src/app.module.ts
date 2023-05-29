0ac88f662578c1dcaaf7ba226876025dfb473485
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-05-29 14:47:12 +0300

    feat(backend): added fastify to host, remove @nestjs/serve-static

--- projects/backend/host/src/app.module.ts
+++ projects/backend/host/src/app.module.ts
@@ -2,9 +2,8 @@ import { Module } from "@nestjs/common";
 import { ComicModule } from "./modules";
 import { ConfigurationModule } from "@cjp-back/configuration";
 import { MongoModule } from "@cjp-back/mongo";
-import { StaticFilesModule } from "@cjp-back/static-files";
 
 @Module({
-  imports: [ConfigurationModule, MongoModule, ComicModule, StaticFilesModule],
+  imports: [ConfigurationModule, MongoModule, ComicModule],
 })
 export class AppModule {}
