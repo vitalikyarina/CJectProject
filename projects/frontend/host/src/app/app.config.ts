7932651664da574a3c9689d58dab8fadeec20f65
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-07-04 17:53:03 +0300

    refactor(frontend-host): added index.ts in app folder

--- projects/frontend/host/src/app/app.config.ts
+++ projects/frontend/host/src/app/app.config.ts
@@ -1,11 +1,10 @@
-import { ApplicationConfig, importProvidersFrom } from "@angular/core";
+import { ApplicationConfig } from "@angular/core";
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from "@angular/router";
 import { provideHttpClient } from "@angular/common/http";
 import { provideAnimations } from "@angular/platform-browser/animations";
-import { NgxsModule } from "@ngxs/store";
 
 import { routes } from "./app.routes";
 
@@ -14,6 +13,5 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideRouter(routes, withEnabledBlockingInitialNavigation()),
     provideAnimations(),
-    importProvidersFrom(NgxsModule.forRoot([])),
   ],
 };
