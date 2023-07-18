981c42989ae11ddbc010e76c160ac8a022719a88
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2023-07-18 17:58:06 +0300

    feat(backend-comic): added microservice comic

---
+++ projects/backend/comic/src/main.ts
@@ -0,0 +1,24 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { Transport, MicroserviceOptions } from "@nestjs/microservices";
+
+import { AppModule } from "./app/app.module";
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: { port: 3001 },
+    },
+  );
+  await app.listen();
+  Logger.log(`🚀 Application of comic is running`);
+}
+
+bootstrap();
