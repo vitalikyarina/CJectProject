803a475c6647e9607b35839a08cece1624eb1d11
Vitalik <vitalikyarina.1999@gmail.com>
2023-07-25 01:53:08 +0300

    feat(backend): removed rxjs from mongo apis, used promise

--- projects/backend/libs/mongo/src/lib/core/services/base-mongo.service.ts
+++ projects/backend/libs/mongo/src/lib/core/services/base-mongo.service.ts
@@ -1,5 +1,4 @@
 import { FilterQuery } from "mongoose";
-import { Observable } from "rxjs";
 import { BaseMongoAPIService } from "../api";
 import { IBaseDatabaseDAO, IFindOptions } from "@cjp-back/shared";
 
@@ -10,33 +9,33 @@ export class BaseMongoService<TDocument, TCreate, TUpdate>
     private readonly baseApi: BaseMongoAPIService<TDocument, TCreate, TUpdate>,
   ) {}
 
-  public createOne(newDocument: TCreate): Observable<TDocument> {
+  public createOne(newDocument: TCreate): Promise<TDocument> {
     return this.baseApi.createOne(newDocument);
   }
 
   public find(
     queryConditions?: FilterQuery<TDocument>,
     options?: IFindOptions,
-  ): Observable<TDocument> {
+  ): Promise<TDocument[]> {
     return this.baseApi.find(queryConditions, options);
   }
 
-  public findById(id: string): Observable<TDocument> {
+  public findById(id: string): Promise<TDocument> {
     return this.baseApi.findById(id);
   }
 
-  public deleteOne(id: string): Observable<TDocument> {
+  public deleteOne(id: string): Promise<TDocument> {
     return this.baseApi.deleteOne(id);
   }
 
   public updateOne(
     queryConditions: FilterQuery<TDocument>,
     updateData: TUpdate,
-  ): Observable<TDocument> {
+  ): Promise<TDocument> {
     return this.baseApi.updateOne(queryConditions, updateData);
   }
 
-  public updateOneById(id: string, updateData: TUpdate): Observable<TDocument> {
+  public updateOneById(id: string, updateData: TUpdate): Promise<TDocument> {
     return this.baseApi.updateOneById(id, updateData);
   }
 }
